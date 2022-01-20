eebc457640e2bda8f969fdbcd5bbcb276847d55b
박재현 <jehyn923@naver.com>
2022-01-20 23:30:54 +0900

    Feat: initial connection-status commit

---
+++ backend-java/connection-status/src/main/java/com/lie/connectionstatus/ConnectionStatusApplication.java
@@ -0,0 +1,13 @@
+package com.lie.connectionstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectionStatusApplication.class, args);
+	}
+
+}
---
+++ backend-java/connection-status/src/test/java/com/lie/connectionstatus/ConnectionStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lie.connectionstatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
