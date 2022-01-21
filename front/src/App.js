422c3da41b02489f8b664bf4d0afa79b8db2f747
Younwoo Lee <leeyw2709@gmail.com>
2022-01-21 17:56:32 +0900

    Feat: 방 입장

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NickName from "./pages/NickName";
+import Room from "./pages/Room";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/nickname/:roomID" component={NickName}>
+        <NickName />
+      </Route>
+      <Route path="/room/:roomID/" component={Room}>
+        <Room />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
---
+++ front/src/pages/Room.js
@@ -0,0 +1,4 @@
+function Room() {
+  return <h1>방</h1>;
+}
+export default Room;
