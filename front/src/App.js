9628b3eaf37edf6648d556075d7c299e4f85605d
Younwoo Lee <leeyw2709@gmail.com>
2022-01-22 15:53:03 +0900

    Feat: Reset before merge

--- front/src/App.js
+++ front/src/App.js
@@ -1,27 +1,7 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import Home from "./pages/Home";
-import NickName from "./pages/NickName";
-import Room from "./pages/Room";
-import MyVideo from "./pages/MyVideo";
 
 function App() {
-  return (
-    <div>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/nickname/:roomID" component={NickName}>
-        <NickName />
-      </Route>
-      <Route path="/room/:roomID/" component={Room}>
-        <Room />
-      </Route>
-      <Route path="/myvideo" component={MyVideo}>
-        <MyVideo />
-      </Route>
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
