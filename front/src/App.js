57bc1e1c4e61f63e074735f022d16443e9af834d
seongbiny <s30ngb1n@gmail.com>
2022-01-24 19:30:20 +0900

    Docs: Router.md

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,21 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import NickName from "./pages/NickName";
 import Room from "./pages/Room";
-import MyVideo from "./pages/MyVideo";
+import Error from "./pages/Error";
+import GameResult from "./pages/GameResult";
 
 function App() {
   return (
     <div>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/nickname/:roomID" component={NickName}>
-        <NickName />
-      </Route>
-      <Route path="/groupcall" component={Room}>
-        <Room />
-      </Route>
-      <Route path="/myvideo" component={MyVideo}>
-        <MyVideo />
-      </Route>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/Liemafia/:roomId" component={NickName} />
+        <Route path="/groupcall" component={Room} />
+        <Route path="/gameresult" component={GameResult} />
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
--- front/src/pages/NickName.js
+++ front/src/pages/NickName.js
@@ -1,42 +1,59 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+import { useParams } from "react-router";
+import styled from "styled-components";
+
+const Box = styled.div`
+  border: 1px solid;
+  width: 20rem;
+  padding: 30px;
+`;
 
 function NickName() {
   const { roomId } = useParams();
-  const [isConfirmed, setIsConfirmed] = useState(false);
+  console.log(roomId);
+
   const [nickName, setNickName] = useState("");
 
-  const onChangeNickName = (event) => {
+  const onChangeNickName = event => {
     setNickName(event.target.value);
+    //console.log(event.target.value);
   };
 
-  const onClick = (event) => {
-    console.log(nickName);
-    setIsConfirmed(true);
+  const onClick = () => {
+    // axios 통신으로 닉네임 중복확인 보내기
+    axios
+      .get("")
+      .then(response => {
+        console.log(response.data);
+        response.data.map(obj => (
+          <div key={obj.id}>
+            {obj.id !== nickName && <button>입장하기</button>}
+          </div>
+        ));
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
   return (
     <div>
-      <h1>NickName</h1>
-      {isConfirmed ? (
-        <div>
-          <h3>닉네임: {nickName}</h3>
-          <Link to={`/myvideo`}>입장하기</Link>
-        </div>
-      ) : (
-        <div>
-          <label htmlFor="nickName" />
+      <img alt="logo" src="../../img/logo.png" />
+      <Box>
+        <form className="nickname-form">
+          닉네임 :
           <input
             value={nickName}
-            id="nickName"
             type="text"
+            id="nickName"
             onChange={onChangeNickName}
             placeholder="닉네임을 입력하세요."
           />
           <button onClick={onClick}>확인</button>
-        </div>
-      )}
+        </form>
+      </Box>
+      <button>입장하기</button>
     </div>
   );
 }
