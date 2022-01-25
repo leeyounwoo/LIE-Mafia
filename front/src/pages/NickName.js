1cc4f271745299476c5b4072090a0e4a0393faba
seongbiny <s30ngb1n@gmail.com>
2022-01-25 18:37:11 +0900

    Feat: nickname props to room

--- front/src/pages/NickName.js
+++ front/src/pages/NickName.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useParams } from "react-router";
+import { useParams, useHistory } from "react-router-dom";
 import styled from "styled-components";
 
 const Box = styled.div`
@@ -10,31 +10,43 @@ const Box = styled.div`
 `;
 
 function NickName() {
+  let history = useHistory();
+  const [flipped, setFlipped] = useState(false);
   const { roomId } = useParams();
-  console.log(roomId);
+  //console.log(roomId);
 
   const [nickName, setNickName] = useState("");
 
   const onChangeNickName = event => {
     setNickName(event.target.value);
     //console.log(event.target.value);
+    console.log(nickName);
   };
 
+  const onFlip = () => setFlipped(current => !current);
+
   const onClick = () => {
+    history.push({
+      pathname: "/room",
+      state: { nickName: nickName },
+    });
+    //console.log(nickName);
+  };
+
+  const onClickCheck = () => {
     // axios 통신으로 닉네임 중복확인 보내기
-    axios
-      .get("")
-      .then(response => {
-        console.log(response.data);
-        response.data.map(obj => (
-          <div key={obj.id}>
-            {obj.id !== nickName && <button>입장하기</button>}
-          </div>
-        ));
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    // axios
+    //   .get("")
+    //   .then(response => {
+    //     console.log(response.data);
+    //     response.data.map(obj => (
+    //       <div key={obj.id}>{obj.id !== nickName && onFlip}</div>
+    //     ));
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
+    console.log(nickName);
   };
 
   return (
@@ -42,7 +54,7 @@ function NickName() {
       <img alt="logo" src="../../img/logo.png" />
       <Box>
         <form className="nickname-form">
-          닉네임 :
+          <label htmlFor="nickName">닉네임 :</label>
           <input
             value={nickName}
             type="text"
@@ -50,10 +62,12 @@ function NickName() {
             onChange={onChangeNickName}
             placeholder="닉네임을 입력하세요."
           />
-          <button onClick={onClick}>확인</button>
+          <button onClick={onClickCheck}>확인</button>
         </form>
       </Box>
-      <button>입장하기</button>
+      <button disabled={false} onClick={onClick}>
+        입장하기
+      </button>
     </div>
   );
 }
