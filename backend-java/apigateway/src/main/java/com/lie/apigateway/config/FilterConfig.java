a1d241fa6583589fd86f654fedc3f3a531a0f608
ohj4312@naver.com <ohj4312@naver.com>
2022-01-26 16:18:20 +0900

    feat : local routing test

---
+++ backend-java/apigateway/src/main/java/com/lie/apigateway/config/FilterConfig.java
@@ -0,0 +1,16 @@
+package com.lie.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public RouteLocator getewayRoutes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r->r.path("/chat/**").uri("http://localhost:8080"))
+                .build();
+    }
+}
