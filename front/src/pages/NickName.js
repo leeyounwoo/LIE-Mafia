8fe07183b6cc957a892bb95a3a175bfec426ec65
seongbiny <s30ngb1n@gmail.com>
2022-01-26 14:32:49 +0900

    Feat: enter-room

--- front/src/pages/NickName.js
+++ front/src/pages/NickName.js
@@ -10,22 +10,26 @@ const Box = styled.div`
 `;
 
 function NickName() {
+  const socket = new WebSocket("ws://i6c209.p.ssafy.io:8080/connect");
   let history = useHistory();
-
-  const socket = new WebSocket("wss://3.37.1.251:8443/groupcall");
-
-  useEffect(() => {
-    // console.log(nickName);
-    socket.onopen = event => {
-      console.log("연결");
-    };
-    return socket.close();
-  }, []);
-
-  const [flipped, setFlipped] = useState(false);
   let { roomId } = useParams();
 
+  const [roomNum, setRoomNum] = useState("");
   const [nickName, setNickName] = useState("");
+  const [flipped, setFlipped] = useState(false);
+
+  useEffect(() => {
+    console.log(roomId);
+    socket.onopen = () => {
+      console.log("연결");
+    };
+  }, []);
+
+  useEffect(() => {
+    RoomValidCheck();
+  }, [roomId]);
+
+  const onFlip = () => setFlipped(current => !current);
 
   const onChangeNickName = event => {
     setNickName(event.target.value);
@@ -33,48 +37,69 @@ function NickName() {
     // console.log(nickName);
   };
 
-  const onFlip = () => setFlipped(current => !current);
-
-  const onClick = () => {
-    socket.send(
-      JSON.stringify({
-        eventType: "connection",
-        actionType: roomId === "0" ? "createRoom" : "joinRoom",
-        roomId: "",
-        username: nickName,
-      })
-    );
-
-    console.log(
-      JSON.stringify({
-        eventType: "connection",
-        actionType: roomId === "0" ? "createRoom" : "joinRoom",
-        roomId: "",
-        username: nickName,
-      })
-    );
-    socket.onmessage = event => {
-      console.log(JSON.parse(event.data));
-    };
+  const onClickEnter = () => {
+    // console.log(roomNum);
     history.push({
-      pathname: "/room",
+      pathname: roomId === "0" ? `/room/${roomNum}` : `/room/${roomId}`,
       state: { nickName: nickName },
     });
   };
-
-  const onClickCheck = () => {
-    // axios 통신으로 닉네임 중복확인 보내기
+  const RoomValidCheck = () => {
     axios
-      .get("url")
+      .get(`http://i6c209.p.ssafy.io:8080/room/${roomId}/`)
       .then(response => {
         console.log(response.data);
-        response.data.map(obj => (
-          <div key={obj.id}>{obj.id !== nickName && onFlip}</div>
-        ));
       })
       .catch(error => {
         console.log(error);
       });
+  };
+  const NickNameCheck = () => {
+    console.log(nickName);
+    axios
+      .get(`http://i6c209.p.ssafy.io:8080/room/${roomId}/username/${nickName}`)
+      // {
+      //   url: `http://i6c209.p.ssafy.io:8080/room/${roomId}/username/${nickName}`,
+      //   method: "get",
+      //   baseURL: "http://localhost:8080",
+      // }
+
+      .then(response => {
+        console.log(response.data);
+        // response.data.map(obj => (
+        //   <div key={obj.id}>{obj.id !== nickName && onFlip}</div>
+        // ));
+      })
+      .catch(error => {
+        console.log(error);
+        console.log("이미 있는 닉네임입니다.");
+      });
+  };
+
+  const onClickCheck = event => {
+    event.preventDefault();
+
+    socket.send(
+      JSON.stringify({
+        eventType: "connection",
+        actionType: roomId === "0" ? "create" : "join",
+        roomId: roomId === "0" ? "" : roomId,
+        username: nickName,
+      })
+    );
+    console.log(
+      JSON.stringify({
+        eventType: "connection",
+        actionType: roomId === "0" ? "create" : "join",
+        roomId: roomId === "0" ? "" : roomId,
+        username: nickName,
+      })
+    );
+    socket.onmessage = event => {
+      console.log(event.data);
+      setRoomNum(event.data);
+    };
+    NickNameCheck();
   };
 
   return (
@@ -93,7 +118,7 @@ function NickName() {
           <button onClick={onClickCheck}>확인</button>
         </form>
       </Box>
-      <button disabled={false} onClick={onClick}>
+      <button disabled={false} onClick={onClickEnter}>
         입장하기
       </button>
     </div>
