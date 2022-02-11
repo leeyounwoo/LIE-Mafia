febedc7262c807a659a6e86043c3cd491cddb2fa
ohj4312@naver.com <ohj4312@naver.com>
2022-02-12 03:19:33 +0900

    Feat : Create chatting room

--- backend-java/chat/src/main/java/com/lie/chat/adapter/ChatHandler.java
+++ backend-java/chat/src/main/java/com/lie/chat/adapter/ChatHandler.java
@@ -1,4 +1,19 @@
 package com.lie.chat.adapter;
 
-public class ChatHandler {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+@Slf4j
+public class ChatHandler extends TextWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        log.info(message.getPayload());
+        //super.handleTextMessage(session, message);
+    }
+
 }
--- backend-java/chat/src/main/java/com/lie/chat/domain/ChatRoom.java
+++ backend-java/chat/src/main/java/com/lie/chat/domain/ChatRoom.java
@@ -1,37 +1,17 @@
 package com.lie.chat.domain;
 
-import com.lie.chat.dto.ChatMessage;
-import com.lie.chat.port.ChatService;
-import lombok.Builder;
-import lombok.Data;
-import org.springframework.web.socket.WebSocketSession;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import java.util.HashMap;
 
 @Data
+@RedisHash(value = "room")
 public class ChatRoom {
-    private String roomId;
-    private List<User> users=new ArrayList<>();
-    private Set<WebSocketSession> sessions=new HashSet<>();
-
-    @Builder
-    public ChatRoom(String roomId){
-        this.roomId=roomId;
-    }
-
-    public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService){
-        if(chatMessage.getType().equals(ChatMessage.MessageType.ENTER)){
-            sessions.add(session); //채팅방에 등록됨
-            chatMessage.setContent(chatMessage.getUsername()+"님이 입장했습니다.");
-        }
-        sendMessage(chatMessage,chatService);
-    }
-
-    public <T> void sendMessage(T chatMessage, ChatService chatService) {
-        sessions.parallelStream().forEach(session -> chatService.sendMessage(session,chatMessage));
-    }
+    @Id
+    String roomId;
+    HashMap<String,User> participants = new HashMap<>();
+    RoomStatus roomStatus;
 
 }
